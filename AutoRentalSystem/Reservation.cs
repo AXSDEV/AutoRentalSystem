4220a983752bbf6453db714c3fcdb47c2f394f2a
devalexmacedo <dev.alexmacedo@gmail.com>
2025-12-11 20:40:29 +0000

    Correção Classe

---
+++ AutoRentalSystem/Reservation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AutoRentalSystem
+{
+	public class Reservation
+	{
+		public int Id { get; set; }
+		public Vehicle Vehicle { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public decimal TotalPrice { get; set; }
+		public bool IsCompleted { get; set; }
+
+		public Reservation(int Id, Vehicle vehicle, DateTime startDate, DateTime endDate)
+		{
+			this.Id = Id;
+			this.Vehicle = vehicle;
+			this.StartDate = startDate;
+			this.EndDate = endDate;
+			this.IsCompleted = false;
+		}
+
+		public void CalculatePrice(decimal baseDailyPrice)
+		{
+			TimeSpan rentalDuration = EndDate - StartDate;
+			int totalDays = (int)Math.Ceiling(rentalDuration.TotalDays);
+
+			if (totalDays < 1) totalDays = 1;
+
+			TotalPrice = totalDays * baseDailyPrice;
+		}
+
+		public override string ToString()
+		{
+			return $"Reservation ID: {Id}, Vehicle: {Vehicle}, " + $"Start: {StartDate:dd/MM/yyyy}, End: {EndDate:dd/MM/yyyy}, " + $"Total: {TotalPrice:C}, Completed: {IsCompleted}";
+		}
+	}
+}
