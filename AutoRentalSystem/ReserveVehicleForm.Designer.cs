0663eb1621811f44b6f5ece7555cb7d8011e4d6b
Gonçalo Matos <g17matos@gmail.com>
2026-01-31 23:20:15 +0000

    ReserveVehicle updated

--- AutoRentalSystem/ReserveVehicleForm.Designer.cs
+++ AutoRentalSystem/ReserveVehicleForm.Designer.cs
@@ -2,15 +2,10 @@
 {
     partial class ReserveVehicleForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,16 +17,13 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReserveVehicleForm));
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.btn_close = new Guna.UI2.WinForms.Guna2Button();
             this.btn_reserveVehicle = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_reserveVehicle.Click += new System.EventHandler(this.btn_reserveVehicle_Click);
             this.dateTimePicker_endDate = new Guna.UI2.WinForms.Guna2DateTimePicker();
             this.dateTimePicker_startDate = new Guna.UI2.WinForms.Guna2DateTimePicker();
             this.textbox_totalPrice = new Guna.UI2.WinForms.Guna2TextBox();
